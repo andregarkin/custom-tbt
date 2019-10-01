6f2f97ca44d5d423705bd16238edb2a02d49c5ee
andregarkin <andregarkin@gmail.com>
2019-10-01 12:24:43 +0300

    initial

---
+++ includes/class-custom-tbt-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wppb.me/
+ * @since      1.0.0
+ *
+ * @package    Custom_Tbt
+ * @subpackage Custom_Tbt/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Tbt
+ * @subpackage Custom_Tbt/includes
+ * @author     Andriy <andregarkin@gmail.com>
+ */
+class Custom_Tbt_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-tbt-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wppb.me/
+ * @since      1.0.0
+ *
+ * @package    Custom_Tbt
+ * @subpackage Custom_Tbt/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Tbt
+ * @subpackage Custom_Tbt/includes
+ * @author     Andriy <andregarkin@gmail.com>
+ */
+class Custom_Tbt_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
